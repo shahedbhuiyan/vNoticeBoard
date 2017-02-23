67ebc50dae4c0c1def1308afcd24dfdf6864392a
= <shahed.blackmagic@gmail.com>
2017-02-22 21:34:52 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# vNoticeBoard
---
+++ src/main/java/com/livingoncodes/vnoticeboard/VNoticeBoardApplication.java
@@ -0,0 +1,11 @@
+package com.livingoncodes.vnoticeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VNoticeBoardApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(VNoticeBoardApplication.class, args);
+	}
+}
